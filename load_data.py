9c3cd899f2ea6a659e648eb124cacf984740119a
Fenne <fennebosman@hotmail.com>
2026-03-03 12:25:00 +0100

    Bekijken van de data 1

--- load_data.py
+++ load_data.py
@@ -1,5 +1,9 @@
+#%%
+
 import pandas as pd
 import os
+
+path = ("/Users/fenne/Documents/Technical Medicine/TM10011/Project/group16_TM10011/Lipo_radiomicFeatures.csv")
 
 def load_data():
     this_directory = os.path.dirname(os.path.abspath(__file__))
@@ -7,3 +11,28 @@ def load_data():
 
     return data
 
+
+
+
+# %% feature summary
+feature_summary = {}
+
+for col in data.columns:
+    if "_sf_" in col:
+        base = col.split("_sf_")[1].split("_")[0]
+        stat = col.split(base + "_")[1].split("_")[0]
+        
+        if base not in feature_summary:
+            feature_summary[base] = []
+            
+        feature_summary[base].append(stat)
+
+for k, v in feature_summary.items():
+    print(k, ":", sorted(v))
+# %% printing verdeling
+lipoma = data[data["label"] == "lipoma"]
+liposarcoma = data[data["label"] == "liposarcoma"]
+
+print("Lipoma:", lipoma.shape)
+print("Liposarcoma:", liposarcoma.shape)
+# %%
