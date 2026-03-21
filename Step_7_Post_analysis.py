976e519e487859193284f162542dde7931f1e638
mfvanhoffe-wq <mfvanhoffe@gmail.com>
2026-03-21 14:38:05 +0100

    Including ROC curve in post-analysis

--- Step_7_Post_analysis.py
+++ Step_7_Post_analysis.py
@@ -8,12 +8,15 @@
 
 def outcomes(y_test, y_score, y_pred, model):
     from sklearn import metrics
+    import matplotlib.pyplot as plt
 
     auc=metrics.roc_auc_score(y_test, y_score)
     accuracy=metrics.accuracy_score(y_test, y_pred)
     F1=metrics.f1_score(y_test,y_pred)
     precision=metrics.precision_score(y_test,y_pred)
     recall=metrics.recall_score(y_test, y_pred)
+    fpr, tpr, thresholds = metrics.roc_curve(y_test, y_score)
+
 # accuracy, AUC, f1score, precision, recall
     print(type(model))
     print('Acc:' +str(accuracy))
@@ -22,4 +25,16 @@ def outcomes(y_test, y_score, y_pred, model):
     print('precision:' +str(precision))
     print('recall:' +str(recall))
 
+    # ROC Curve
+
+    plt.figure()
+    plt.plot(fpr, tpr, label='ROC curve (AUC = %0.2f)' % auc)
+    plt.plot([0, 1], [0, 1], linestyle='--')  # diagonal line
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc='lower right')
+    plt.show()
+
     return auc, accuracy, F1, precision, recall
+# %%
